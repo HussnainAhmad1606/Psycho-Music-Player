7b284f4105bd4d98b18971d1b27fc24d48bf9da0
Hussnain Ahmad <75199596+HussnainAhmad1606@users.noreply.github.com>
2022-10-20 12:16:25 +0500

    Next and previous button added
    Next and previous button is working fine. but Previous button is not working when first track is playing in the GUI

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 from tkinter import *
 import os
+from mutagen.mp3 import MP3
 import pygame
+from multiprocessing import Process
+
 root = Tk()
 
 root.title("Psycho Music Player")
 root.geometry("300x300")
-musics = os.listdir()
+listDir = os.listdir()
+
+musics = []
+
+for item in listDir:
+	if ".mp3" in item or ".MP3" in item:
+		print(item)
+		musics.append(item)
+
+print(musics)
 soundObj = None
 currentMusicIndex = 0
 volumeVar = DoubleVar()
 volumeVar.set(100.0)
+
 
 def pauseMusic():
 	pygame.mixer.music.pause()
@@ -22,15 +35,34 @@ def resumeMusic():
 def initialMusic():
 	print("Initialization Done!")
 	global soundObj
-	global currentIndex
+	global currentMusicIndex
 	pygame.init()
 	soundObj =  pygame.mixer.music.load(musics[currentMusicIndex])
-
 initialMusic()
 
 def playMusic():
 	pygame.mixer.music.play()
 
+def previousTrack():
+	global currentMusicIndex
+	if currentMusicIndex == 0:
+		currentMusicIndex = len(musics)
+	else:
+		currentMusicIndex -= 1
+	initialMusic()
+	playMusic()
+	musicName.config(text=musics[currentMusicIndex])
+
+
+def nextTrack():
+	global currentMusicIndex
+	if (len(musics)-1) == currentMusicIndex:
+		currentMusicIndex = 0
+	else:
+		currentMusicIndex += 1
+	initialMusic()
+	playMusic()
+	musicName.config(text=musics[currentMusicIndex])
 
 def changeVolume(event):
 	pygame.mixer.music.set_volume(volume.get()/100)
@@ -39,8 +71,9 @@ def changeVolume(event):
 title = Label(root, text="Music Player", font=("Calibri", 20, "bold"))
 title.pack()
 
-music = Label(root, text=musics[currentIndex])
-music.pack()
+musicName = Label(root, text=musics[currentMusicIndex])
+musicName.pack()
+
 
 playBtn = Button(root, text="Play", command=playMusic)
 playBtn.pack()
@@ -50,6 +83,13 @@ pauseBtn = Button(root, text="Pause", command=pauseMusic)
 
 pauseBtn.pack()
 
+
+prevTrackBtn = Button(root, text="Previous Track", command=previousTrack)
+prevTrackBtn.pack()
+
+
+nextTrackBtn = Button(root, text="Next Track", command=nextTrack)
+nextTrackBtn.pack()
 
 volume = Scale(
     root,
